daf355de34a4458520686c0042a9fa704b9e611c
rafaelmena98 <rafaelmenamejia98@gmail.com>
2026-05-31 18:56:58 -0600

    img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CartComponent } from './components/cart/cart.component';
